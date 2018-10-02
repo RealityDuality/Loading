0f29ab0ff33afb106d080c5cbca6ad875861f541
Duality <sorrowsflight@gmail.com>
2018-10-02 15:02:16 +1300

    Updated Math

--- Assets/Progression.cs
+++ Assets/Progression.cs
@@ -27,26 +27,30 @@ private float keyStrokes;
     [SerializeField]
     private float ActiveTime;
 
+	[SerializeField]
+	private float MinSpeedToFinish;
 
+	[SerializeField]
+	private float SpeedToFinish; //Make sure it is incredibly small.
+
+	[SerializeField]
+	private float MaxSpeedToFinish;
 
 
 
 
     // Use this for initialization
     void Start () {
-
+		//AddTime = SpeedToFinish;
+		MinSpeedToFinish = SpeedToFinish;
 	}
 
 	// Update is called once per frame
 	void Update () {
-                ActiveTime += Time.deltaTime;
+		ActiveTime += Time.deltaTime / SpeedToFinish;
                 var percent = ActiveTime / TimeToFinish;
                 LoadBar.fillAmount = Mathf.Lerp(0, 1, percent);
 
-        if(TimeToFinish >= MaxTimeToFinish)
-        {
-            TimeToFinish = MaxTimeToFinish;
-        }
           
 
 
@@ -54,10 +58,20 @@ private float keyStrokes;
 			{
 	 	        KeyPressed();
 
-            TimeToFinish += AddTime;
+            //TimeToFinish += AddTime;
             //ActiveTime -= AddTime;
+			SpeedToFinish += AddTime;
 
 			}
+
+		if (SpeedToFinish >= MinSpeedToFinish) {
+			SpeedToFinish -= Time.deltaTime / SpeedToFinish;
+
+		}
+		if (SpeedToFinish >= MaxTimeToFinish) {
+			SpeedToFinish = MaxTimeToFinish;
+		}
+
 
 	 }
 
