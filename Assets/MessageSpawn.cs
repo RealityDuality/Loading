920663ee914824e255e52751225c284d209c1b4e
Duality <sorrowsflight@gmail.com>
2018-10-02 16:46:29 +1300

    Updated System

---
+++ Assets/MessageSpawn.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageSpawn : MonoBehaviour {
+
+    public GameObject message1;
+    public GameObject message2;
+    public GameObject message3;
+    public GameObject message4;
+    public GameObject message5;
+    public int randomSpawn;
+    public GameObject[] messages;
+
+
+    // Use this for initialization
+    void Start () {
+
+        StartCoroutine(SpawnMessage());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        messages = GameObject.FindGameObjectsWithTag("message");
+
+        if (Input.anyKeyDown)
+        {
+            foreach(GameObject message in messages)
+            {
+                Destroy(message);
+            }
+
+            //StartCoroutine(SpawnMessage());
+        }
+
+   	}
+
+    IEnumerator SpawnMessage()
+    {
+
+        randomSpawn = Random.Range(1, 6);
+        float spawnY = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+        float spawnX = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+        Vector2 spawnPosition = new Vector2(spawnX, spawnY);
+        
+        switch (randomSpawn)
+        {
+            case 1:
+                Instantiate(message1, spawnPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+                break;
+            case 2:
+                Instantiate(message2, spawnPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+                break;
+            case 3:
+                Instantiate(message3, spawnPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+                break;
+            case 4:
+                Instantiate(message4, spawnPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+                break;
+            case 5:
+                Instantiate(message5, spawnPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+                break;
+        }
+
+        yield return new WaitForSeconds(0.2f);
+
+        StartCoroutine(SpawnMessage());
+
+    }
+}
--- Assets/Progression.cs
+++ Assets/Progression.cs
@@ -6,16 +6,29 @@ using UnityEngine.UI;
 public class Progression : MonoBehaviour {
 
 public Image LoadBar;
+	[SerializeField]
+	private GameObject message1;
+	[SerializeField]
+	private GameObject message2;
+	[SerializeField]
+	private GameObject message3;
+	[SerializeField]
+	private GameObject message4;
+	[SerializeField]
+	private GameObject message5;
+	[SerializeField]
+	private int randomSpawn;
 
+	public GameObject[] messages;
+
+	public bool TimeToSpawn;
+
+	public bool FirstTime;
 
 [SerializeField]
 private float AddTime;
 
-
- 
-
-[SerializeField]
-private float keyStrokes;
+	public float keyStrokes;
 
     [SerializeField]
     private float TimeToFinish;
@@ -36,17 +49,41 @@ private float keyStrokes;
 	[SerializeField]
 	private float MaxSpeedToFinish;
 
+	[SerializeField]
+	private float HalfTime;
 
+	[SerializeField]
+	private float WaitSeconds;
 
+	private float DefaultWaitSeconds;
+
+	[SerializeField]
+	private float RestartTime;
+
+	[SerializeField]
+	private float MaxRestartTime;
+
+	[SerializeField]
+	private float IncBy;
+
+	[SerializeField]
+	private int NumberOfBox;
 
     // Use this for initialization
     void Start () {
 		//AddTime = SpeedToFinish;
 		MinSpeedToFinish = SpeedToFinish;
+		DefaultWaitSeconds = WaitSeconds;
+
+		HalfTime = TimeToFinish / 2;
+		TimeToSpawn = true;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		messages = GameObject.FindGameObjectsWithTag("message");
+
+
 		ActiveTime += Time.deltaTime / SpeedToFinish;
                 var percent = ActiveTime / TimeToFinish;
                 LoadBar.fillAmount = Mathf.Lerp(0, 1, percent);
@@ -64,13 +101,16 @@ private float keyStrokes;
 
 			}
 
+
+
 		if (SpeedToFinish >= MinSpeedToFinish) {
-			SpeedToFinish -= Time.deltaTime / SpeedToFinish;
+			SpeedToFinish -= Time.deltaTime / 64;
 
 		}
-		if (SpeedToFinish >= MaxTimeToFinish) {
-			SpeedToFinish = MaxTimeToFinish;
+		if (SpeedToFinish >= MaxSpeedToFinish) {
+			SpeedToFinish = MaxSpeedToFinish;
 		}
+
 
 
 	 }
@@ -78,7 +118,45 @@ private float keyStrokes;
 
 
 
-    public void KeyPressed() { keyStrokes++;
+	void FixedUpdate(){
+	
+
+
+		if (ActiveTime >= HalfTime) {
+			//TimeToSpawn = true;
+			if (FirstTime == true) {
+				StartCoroutine (SpawnMessage ());
+				FirstTime = false;
+
+			}
+			RestartTime += Time.fixedDeltaTime;
+		}
+
+		if (RestartTime >= MaxRestartTime) {
+			TimeToSpawn = true;
+		}
+
+	}
+
+
+    public void KeyPressed() { 
+		keyStrokes++;
+
+
+
+		if(FirstTime == false){
+		RestartTime = 0;
+			TimeToSpawn = false;}
+
+		if (TimeToSpawn == true) {
+			SpeedToFinish += IncBy;
+		}
+
+		foreach(GameObject message in messages)
+		{
+			Destroy(message);
+			NumberOfBox = 0;
+		}
 
     }
 
@@ -86,7 +164,52 @@ private float keyStrokes;
 
 
 
+	IEnumerator SpawnMessage()
+	{
 
+		randomSpawn = Random.Range(1, 6);
+		float spawnY = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+		float spawnX = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+		Vector2 spawnPosition = new Vector2(spawnX, spawnY);
+
+		if (TimeToSpawn == true) {
+			switch (randomSpawn) {
+			case 1:
+				Instantiate (message1, spawnPosition, Quaternion.Euler (new Vector3 (0, 0, Random.Range (0, 360))));
+				NumberOfBox += 1;
+				break;
+			case 2:
+				Instantiate (message2, spawnPosition, Quaternion.Euler (new Vector3 (0, 0, Random.Range (0, 360))));
+				NumberOfBox += 1;
+				break;
+			case 3:
+				Instantiate (message3, spawnPosition, Quaternion.Euler (new Vector3 (0, 0, Random.Range (0, 360))));
+				NumberOfBox += 1;
+				break;
+			case 4:
+				Instantiate (message4, spawnPosition, Quaternion.Euler (new Vector3 (0, 0, Random.Range (0, 360))));
+				NumberOfBox += 1;
+				break;
+			case 5:
+				Instantiate (message5, spawnPosition, Quaternion.Euler (new Vector3 (0, 0, Random.Range (0, 360))));
+				NumberOfBox += 1;
+				break;
+			}
+		}
+
+		if (NumberOfBox == 20) {
+			WaitSeconds -= 0.2f;
+		} else if (NumberOfBox == 50) {
+			WaitSeconds -= 0.2f;
+		} else if (NumberOfBox == 0) {
+			WaitSeconds = DefaultWaitSeconds;
+		}
+
+		yield return new WaitForSeconds(WaitSeconds);
+		StartCoroutine(SpawnMessage());
+
+
+	}
 
 
 
